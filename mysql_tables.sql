30941fdd02b9583e2f6c55bddaf3492a5134fc18
Adrian Laurenzi <alaurenz@uw.edu>
2013-01-31 16:52:05 -0800

    Update mysql_tables.sql
    Added missing create table statements

--- mysql_tables.sql
+++ mysql_tables.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.2
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2010 at 04:02 AM
--- Server version: 5.1.37
--- PHP Version: 5.2.10-2ubuntu6.4
+-- Generation Time: Jan 31, 2013 at 04:47 PM
+-- Server version: 5.5.29
+-- PHP Version: 5.3.10-1ubuntu3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -15,13 +16,17 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+--
+-- Database: `impact_probe`
+--
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `active_api_sources`
 --
 
-CREATE TABLE IF NOT EXISTS `active_api_sources` (
+CREATE TABLE `active_api_sources` (
   `api_id` smallint(5) NOT NULL,
   `project_id` int(10) NOT NULL,
   PRIMARY KEY (`api_id`,`project_id`)
@@ -33,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `active_api_sources` (
 -- Table structure for table `api_metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `api_metadata` (
+CREATE TABLE `api_metadata` (
   `api_id` smallint(5) unsigned NOT NULL,
   `name` varchar(50) NOT NULL,
   `value` varchar(500) NOT NULL,
@@ -46,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `api_metadata` (
 -- Table structure for table `api_sources`
 --
 
-CREATE TABLE IF NOT EXISTS `api_sources` (
+CREATE TABLE `api_sources` (
   `api_id` smallint(5) NOT NULL AUTO_INCREMENT,
   `api_name` varchar(60) NOT NULL,
   `gather_method_name` varchar(40) NOT NULL,
@@ -67,7 +72,7 @@ INSERT INTO `api_sources` (`api_id`, `api_name`, `gather_method_name`) VALUES
 -- Table structure for table `cached_text`
 --
 
-CREATE TABLE IF NOT EXISTS `cached_text` (
+CREATE TABLE `cached_text` (
   `meta_id` int(10) NOT NULL,
   `text` mediumtext NOT NULL,
   PRIMARY KEY (`meta_id`)
@@ -79,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `cached_text` (
 -- Table structure for table `cluster_log`
 --
 
-CREATE TABLE IF NOT EXISTS `cluster_log` (
+CREATE TABLE `cluster_log` (
   `project_id` int(10) unsigned NOT NULL,
   `threshold` float(3,3) NOT NULL DEFAULT '0.250',
   `order` varchar(35) NOT NULL DEFAULT 'arbitrarily',
@@ -94,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `cluster_log` (
 -- Table structure for table `doc_clusters`
 --
 
-CREATE TABLE IF NOT EXISTS `doc_clusters` (
+CREATE TABLE `doc_clusters` (
   `meta_id` int(10) unsigned NOT NULL,
   `cluster_id` int(10) unsigned NOT NULL,
   `score` float(6,6) NOT NULL,
@@ -108,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `doc_clusters` (
 -- Table structure for table `doc_clusters_exact`
 --
 
-CREATE TABLE IF NOT EXISTS `doc_clusters_exact` (
+CREATE TABLE `doc_clusters_exact` (
   `meta_id` int(10) unsigned NOT NULL,
   `cluster_id_exact` int(10) unsigned NOT NULL,
   PRIMARY KEY (`meta_id`)
@@ -117,15 +122,43 @@ CREATE TABLE IF NOT EXISTS `doc_clusters_exact` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `doc_clusters_time`
+--
+
+CREATE TABLE `doc_clusters_time` (
+  `meta_id` int(10) unsigned NOT NULL,
+  `cluster_id` int(10) unsigned NOT NULL,
+  `time_plot_id` int(8) NOT NULL,
+  `score` float(6,6) NOT NULL,
+  `project_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`meta_id`,`time_plot_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doc_clusters_time_exact`
+--
+
+CREATE TABLE `doc_clusters_time_exact` (
+  `meta_id` int(10) unsigned NOT NULL,
+  `cluster_id_exact` int(10) unsigned NOT NULL,
+  `time_plot_id` int(8) NOT NULL,
+  PRIMARY KEY (`meta_id`,`time_plot_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `gather_log`
 --
 
-CREATE TABLE IF NOT EXISTS `gather_log` (
+CREATE TABLE `gather_log` (
   `log_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(10) unsigned NOT NULL,
   `date` int(10) unsigned NOT NULL,
   PRIMARY KEY (`log_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -133,14 +166,14 @@ CREATE TABLE IF NOT EXISTS `gather_log` (
 -- Table structure for table `gather_queries`
 --
 
-CREATE TABLE IF NOT EXISTS `gather_queries` (
+CREATE TABLE `gather_queries` (
   `query_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `log_id` int(10) unsigned NOT NULL,
   `search_query` varchar(600) NOT NULL,
   `results_gathered` mediumint(6) unsigned NOT NULL,
   `error` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`query_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -148,7 +181,7 @@ CREATE TABLE IF NOT EXISTS `gather_queries` (
 -- Table structure for table `keywords_phrases`
 --
 
-CREATE TABLE IF NOT EXISTS `keywords_phrases` (
+CREATE TABLE `keywords_phrases` (
   `keyword_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(10) unsigned NOT NULL,
   `keyword_phrase` varchar(250) NOT NULL,
@@ -165,7 +198,7 @@ CREATE TABLE IF NOT EXISTS `keywords_phrases` (
 -- Table structure for table `keyword_metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `keyword_metadata` (
+CREATE TABLE `keyword_metadata` (
   `meta_id` int(10) unsigned NOT NULL,
   `keyword_id` int(10) unsigned NOT NULL,
   `num_occurrences` int(8) unsigned NOT NULL,
@@ -178,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `keyword_metadata` (
 -- Table structure for table `mark_negative_keywords`
 --
 
-CREATE TABLE IF NOT EXISTS `mark_negative_keywords` (
+CREATE TABLE `mark_negative_keywords` (
   `meta_id` int(10) unsigned NOT NULL,
   `project_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`meta_id`)
@@ -190,7 +223,7 @@ CREATE TABLE IF NOT EXISTS `mark_negative_keywords` (
 -- Table structure for table `metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `metadata` (
+CREATE TABLE `metadata` (
   `meta_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(10) unsigned NOT NULL,
   `url_id` int(10) unsigned NOT NULL,
@@ -209,7 +242,7 @@ CREATE TABLE IF NOT EXISTS `metadata` (
 -- Table structure for table `metadata_urls`
 --
 
-CREATE TABLE IF NOT EXISTS `metadata_urls` (
+CREATE TABLE `metadata_urls` (
   `url_id` int(10) NOT NULL AUTO_INCREMENT,
   `url` varchar(600) NOT NULL,
   `project_id` int(10) unsigned NOT NULL,
@@ -219,10 +252,37 @@ CREATE TABLE IF NOT EXISTS `metadata_urls` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `preloaded_rss_feeds`
+--
+
+CREATE TABLE `preloaded_rss_feeds` (
+  `feed_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cat_id` int(10) unsigned NOT NULL,
+  `feed_name` varchar(100) NOT NULL,
+  `is_searchable` tinyint(1) unsigned NOT NULL,
+  `url` varchar(600) NOT NULL,
+  PRIMARY KEY (`feed_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `preloaded_rss_feed_cats`
+--
+
+CREATE TABLE `preloaded_rss_feed_cats` (
+  `cat_id` int(8) NOT NULL AUTO_INCREMENT,
+  `cat_name` varchar(100) NOT NULL,
+  PRIMARY KEY (`cat_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `projects`
 --
 
-CREATE TABLE IF NOT EXISTS `projects` (
+CREATE TABLE `projects` (
   `project_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_title` varchar(120) NOT NULL,
   `date_created` int(10) NOT NULL,
@@ -237,7 +297,7 @@ CREATE TABLE IF NOT EXISTS `projects` (
 -- Table structure for table `rss_feeds`
 --
 
-CREATE TABLE IF NOT EXISTS `rss_feeds` (
+CREATE TABLE `rss_feeds` (
   `feed_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(10) unsigned NOT NULL,
   `date_added` int(10) unsigned NOT NULL,
@@ -246,3 +306,7 @@ CREATE TABLE IF NOT EXISTS `rss_feeds` (
   `active` tinyint(1) NOT NULL,
   PRIMARY KEY (`feed_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
